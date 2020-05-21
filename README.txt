72b6b311876345e220009ad5a28962e2791857c4
Coralie Helm <helm.coralie@gmail.com>
2020-05-20 22:36:57 -0400

    First Commit

---
+++ README.txt
@@ -0,0 +1,3 @@
+#Created on May 19 2020
+
+#Introduction to Turtle via Python Basics presented by the Univeristy of Michigan
--- my_first_turtle.py
+++ my_first_turtle.py
@@ -1,6 +1,22 @@
+#My First Turtle Program
+#Coursera - Python Basics - presented by University of Michigan
+
+
 import turtle             # allows us to use the turtles library
-wn = turtle.Screen()      # creates a graphics window
-alex = turtle.Turtle()    # create a turtle named alex
-alex.forward(150)         # tell alex to move forward by 150 units
-alex.left(90)             # turn by 90 degrees
-alex.forward(75)          # complete the second side of a rectangle
+
+background_color = input("Please, select a Background Color -> ")
+turtle_color = input("Please, select the color of the Turtle -> ")
+pensizing = input("Please, select the size of the Pen from 1 - 20 -> ")
+
+wn = turtle.Screen()     # creates a graphics window
+wn.bgcolor(background_color)
+
+sam = turtle.Turtle()    # create a turtle named sam
+sam.color(turtle_color)
+sam.pensize(pensizing)
+
+sam.forward(150)         # tell sam to move forward by 150 units
+sam.left(90)             # turn by 90 degrees
+sam.forward(75)          # complete the second side of a rectangle
+
+wn.exitonclick()         # wait for a user click on the canvas
