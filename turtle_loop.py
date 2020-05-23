e7c35d549c0f34ffd0369d3234428efef98e27e6
Coralie Helm <helm.coralie@gmail.com>
2020-05-23 13:27:06 -0400

    Added color turtle on turtle_loop.py

--- turtle_loop.py
+++ turtle_loop.py
@@ -12,13 +12,17 @@ elan = turtle.Turtle()
 elan.pensize(3)
 elan.speed(3)
 
-
 claire = turtle.Turtle()
 claire.color("Hotpink")
 claire.pensize(5)
 claire.speed(5)
 
-            
+color_turtle = turtle.Turtle()
+color_turtle.pensize(8)
+c_distance = 35
+
+
+
 
 distance = 50
 for i in range(10):
@@ -26,9 +30,13 @@ for i in range(10):
     elan.right(90)
     distance = distance + 10
 
+for aColor in ["yellow", "pink", "blue", "green"]:
+    color_turtle.color(aColor)
+    color_turtle.left(90)
+    color_turtle.forward(c_distance)
+    distance = distance + 15
+
 for i2 in range(20):
     claire.left(90)
     claire.forward(distance)
     distance = distance + 15
-
-
